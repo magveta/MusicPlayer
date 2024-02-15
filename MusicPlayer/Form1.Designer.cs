a17f6f4460ecf39894a456f7d3c0bffe77e84535
magveta <kaminari061@gmail.com>
2024-02-15 10:51:04 -0300

    Add project files.

---
+++ MusicPlayer/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace MusicPlayer
+{
+    partial class MusicPlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MusicPlayer));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.songNameLabel = new System.Windows.Forms.Label();
+            this.albumNameLabel = new System.Windows.Forms.Label();
+            this.artistNameLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 438);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(759, 45);
+            this.axWindowsMediaPlayer1.TabIndex = 1;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(651, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select song";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(420, 420);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // songNameLabel
+            // 
+            this.songNameLabel.AutoSize = true;
+            this.songNameLabel.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.songNameLabel.Location = new System.Drawing.Point(438, 24);
+            this.songNameLabel.Name = "songNameLabel";
+            this.songNameLabel.Padding = new System.Windows.Forms.Padding(0, 0, 0, 10);
+            this.songNameLabel.Size = new System.Drawing.Size(77, 35);
+            this.songNameLabel.TabIndex = 6;
+            this.songNameLabel.Text = "";
+            // 
+            // albumNameLabel
+            // 
+            this.albumNameLabel.AutoSize = true;
+            this.albumNameLabel.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.albumNameLabel.Location = new System.Drawing.Point(438, 78);
+            this.albumNameLabel.Name = "albumNameLabel";
+            this.albumNameLabel.Size = new System.Drawing.Size(55, 16);
+            this.albumNameLabel.TabIndex = 7;
+            this.albumNameLabel.Text = "";
+            // 
+            // artistNameLabel
+            // 
+            this.artistNameLabel.AutoSize = true;
+            this.artistNameLabel.Font = new System.Drawing.Font("Verdana", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.artistNameLabel.Location = new System.Drawing.Point(440, 59);
+            this.artistNameLabel.Name = "artistNameLabel";
+            this.artistNameLabel.Padding = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.artistNameLabel.Size = new System.Drawing.Size(43, 19);
+            this.artistNameLabel.TabIndex = 8;
+            this.artistNameLabel.Text = "";
+            // 
+            // MusicPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 487);
+            this.Controls.Add(this.artistNameLabel);
+            this.Controls.Add(this.albumNameLabel);
+            this.Controls.Add(this.songNameLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MusicPlayer";
+            this.Text = "MusicPlayer";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label songNameLabel;
+        private System.Windows.Forms.Label albumNameLabel;
+        private System.Windows.Forms.Label artistNameLabel;
+    }
+}
+
---
+++ MusicPlayer/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Configuration;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MusicPlayer
+{
+    public partial class MusicPlayer : Form
+    {
+
+        public MusicPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = pictureBox1.InitialImage;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Music files|*.mp3;*.flac;*.wav";
+
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                axWindowsMediaPlayer1.URL = selectedFilePath;
+
+                TagLib.File f = TagLib.File.Create(selectedFilePath);
+
+                songNameLabel.Text = f.Tag.Title.ToString();
+                if (f.Tag.Album != null | f.Tag.Year != null)
+                {
+                    var album = f.Tag.Album.ToString();
+                    var year = f.Tag.Year.ToString();
+                    albumNameLabel.Text = album + " - " + year;
+                }
+
+                else { albumNameLabel.Text = ""; }
+
+                if (f.Tag.FirstAlbumArtist != null)
+                {
+                    artistNameLabel.Text = f.Tag.FirstAlbumArtist.ToString();
+                }
+
+                else { artistNameLabel.Text = ""; }
+
+                if (f.Tag.Pictures != null && f.Tag.Pictures.Length != 0)
+                {
+                    var bin = (byte[])(f.Tag.Pictures[0].Data.Data);
+                    pictureBox1.Image = Image.FromStream(new MemoryStream(bin));
+                }
+            }
+        }
+    }
+}
